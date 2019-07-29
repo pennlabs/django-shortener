a0f160c6545eb517a2ff7a3d6e899876e041f93b
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2019-07-29 18:10:49 -0400

    Move to class-based views

--- views.py
+++ views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import get_object_or_404, redirect
+from django.views.generic.base import View
 from shortener.models import Url
 
 
-def index(request, short):
-    url = get_object_or_404(Url, short_id=short)
-    return redirect(url.long_url)
+class RedirectView(View):
+    def get(self, request, short):
+        url = get_object_or_404(Url, short_id=short)
+        return redirect(url.long_url)
