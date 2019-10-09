c1e734bfa338e31e9abc82044d1f2586659e8cac
Armaan Tobaccowalla <13340433+ArmaanT@users.noreply.github.com>
2019-10-09 12:08:56 -0400

    Make get_or_create return a tuple

--- shortener/manager.py
+++ shortener/manager.py
@@ -6,7 +6,7 @@ from django.db import models
 class UrlManager(models.Manager):
     def get_or_create(self, long_url):
         if self.filter(long_url=long_url).exists():  # If a shortened URL already exists, don't make a duplicate.
-            return self.get(long_url=long_url)
+            return self.get(long_url=long_url), False
 
         try:
             hashed = hashlib.sha3_256(long_url.encode('utf-8')).hexdigest()
@@ -19,4 +19,4 @@ class UrlManager(models.Manager):
 
         url = self.create(short_id=hashed[:length], long_url=long_url)
         url.save()
-        return url
+        return url, True
