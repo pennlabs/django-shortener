999f5b6d56897396c9ab0a033805738a44209541
Armaan Tobaccowalla <13340433+ArmaanT@users.noreply.github.com>
2019-10-08 22:50:11 -0400

    Update minimum python version to 3.5

--- setup.py
+++ setup.py
@@ -43,12 +43,11 @@ setup(
     classifiers=[
         'Framework :: Django',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
-    python_requires='>=3.4',
+    python_requires='>=3.5',
     cmdclass={
         'verify': VerifyVersionCommand,
     }
